b4c0fd91af8f30ae811d4bb95d8154d796c8ebb2
hlud6646 <hugo.ludemann@gmail.com>
2019-07-19 13:10:54 +1000

    Create game_of_life.py

---
+++ game_of_life.py
@@ -0,0 +1,72 @@
+import numpy as np
+from scipy.signal import convolve2d
+import time
+from itertools import product
+
+def init_cells():
+	height, width = 36, 72
+	cells = np.empty( (height, width), dtype=[  ('state', 	bool, 	1),
+                                              ('age', 	int, 	1),
+                                              ('color', 	float, 	3)])
+	cells['state'] = np.random.choice([0, 1], (height, width), p=[.7, .3])
+	return cells
+
+def update(state):
+	kernel = np.array([1, 1, 1, 1, 0, 1, 1, 1, 1]).reshape((3, 3))
+	neighbours = convolve2d(state, kernel, mode='same')
+
+	death_rule = np.logical_and(state, np.logical_or(neighbours<2, neighbours>3))
+	birth_rule = np.logical_and(~state,neighbours==3)
+	lives_rule = np.logical_and(state, np.logical_or(neighbours==2, neighbours==3))
+
+	return np.where( death_rule, 0,
+				 np.where( lives_rule, 1,
+			  			np.where(birth_rule, 1, state)))
+
+def plot(cells, canvas, color_factor=200):
+	""" Depending on how the image is rendered, colors should be floats in (0, 1) or (0, 255). """
+	height, width   = canvas.shape[:2]
+	gheight, gwidth = cells.shape
+	d = height//gheight
+	r = d//3 # radius of a cell
+	for x, y in product(range(gwidth), range(gheight)):
+		if not cells['state'][y][x]:
+			continue
+		X = (x*d) + d//2
+		Y = (y*d) + d//2
+		canvas[Y-r:Y+r, X-r:X+r] = np.random.uniform(.3, .9, 3)*color_factor
+ 
+def gen(height, width, color_factor=255):
+	cells  = init_cells()
+	canvas = np.zeros((height, width, 3), np.float32)
+	cache  = []
+
+	while True:
+		canvas *= 0.1
+		new_state = update(cells['state'])
+
+		if not np.any(new_state - cells['state']):
+			cells = init_cells()
+			cache = []
+		elif not all( np.any(cells['state']^x) for x in cache ):
+			cells['state'] = False
+		else:
+			cache.append(np.copy(cells['state']))
+			cells['state'] = new_state
+
+		plot(cells, canvas)
+		yield canvas
+		time.sleep(.5)
+
+
+if __name__ == "__main__":
+
+	height, width = 360, 720
+
+	import cv2
+	w = cv2.namedWindow("win")
+	for frame in gen(height, width, color_factor=1):
+		cv2.imshow('win', frame)
+		if cv2.waitKey(int(1000/60)) != -1:
+		 	break
+	cv2.destroyAllWindows()
